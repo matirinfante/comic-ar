7bd2e9577276ec9862daa89d123ff8d74b5ad0d2
matiasperalta <matias.peralta@est.fi.uncoma.edu.ar>
2022-09-19 06:45:36 -0300

    added prev and next buttons

--- resources/js/Pages/openlibrary.vue
+++ resources/js/Pages/openlibrary.vue
@@ -5,8 +5,10 @@
             <label for="input">Nombre del tomo:</label>
             <input id="input" type="text">
         </div>
-        <button onclick="getbooks()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buscar</button>
-        <div id="output"></div>
+        <button onclick="getbooks(1)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Buscar</button>
+        <div id="output" val="0"></div>
+        <button id="prev" onclick="getbooks(2)" class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hidden">Anterior</button>
+        <button id="next" onclick="getbooks(3)" class="bg-red-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hidden">Siguiente</button>
     </div>
     
 </template>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -19,17 +19,51 @@
     </body>
 </html>
 <script>
-    function getbooks(){
-        //document.getElementById('output').innerHTML="";
+    function getbooks(option){
         var outP= document.getElementById('output');
         outP.innerHTML="";
+        var att= outP.getAttribute('val');
+        var btnNext=document.getElementById('next');
+        var btnPrev=document.getElementById('prev');
+        var i=0;
+        if (option=="1"){
+            outP.setAttribute('val',0);
+            btnNext.classList.add('hidden');
+            btnPrev.classList.add("hidden");
+        }
+        if (option=="2"){
+            outP.setAttribute('val',(parseInt(att)-1));
+            att= outP.getAttribute('val');
+            i=10*(parseInt(att));
+        }
+        if (option=="3"){
+            outP.setAttribute('val',(parseInt(att)+1));
+            att= outP.getAttribute('val');
+            i=10*(parseInt(att));
+        }
+        var cantVol=i+10;
+        
         fetch("https://openlibrary.org/search.json?q="+document.getElementById('input').value)
         .then(a=>a.json())
-        .then(response=>{for(var i=0; i<10; i++){
+        .then(response=>{
+            //Compruebo la cantidad de volumenes para establecer el estado del boton Siguiente
+            if (cantVol<response.docs.length){
+                btnNext.classList.remove("hidden");
+            }else{
+                cantVol=response.docs.length;   //Para no buscar datos que no existen en el for
+                btnNext.classList.add("hidden");
+            }   
+            //Compruebo la cantidad de volumenes para establecer el estado del boton Anterior
+            if (cantVol>10){
+                btnPrev.classList.remove("hidden");
+            }else{
+                btnPrev.classList.add("hidden");
+            }  
+            outP.innerHTML+='<h1>'+response.docs.length+' coincidencias</h1>';
+            //Genero el codigo con la info de los volumenes
+            for(i; i<cantVol; i++){
                 var title= "";
-                //console.log(response.docs[i].title);
                 title+=response.docs[i].title;
-                //console.log(response.docs[i].editions.docs[0].subtitle);
                 if (!(response.docs[i].editions.docs[0].subtitle==null)){
                     title+=': '+response.docs[i].editions.docs[0].subtitle;
                 }
@@ -43,8 +77,7 @@
                     outP.innerHTML+='Autor: '+response.docs[i].author_name;
                 }
                 outP.innerHTML+='<hr class="border border-sky-500">';
-            }
-                
+            }                    
         })
     }
 </script>
