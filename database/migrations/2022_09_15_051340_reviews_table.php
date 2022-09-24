b73a1bac2ac5429e88193146cdf0c0d6db53b9ce
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-09-24 18:47:29 -0300

    adding avg rating and review changes

--- database/migrations/2022_09_15_051340_reviews_table.php
+++ database/migrations/2022_09_15_051340_reviews_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,8 +12,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('reviews',function(Blueprint $table){
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->integer('rating');
             $table->string('description');
             $table->foreignId('user_id')->constrained();
             $table->foreignId('edition_id')->constrained();
