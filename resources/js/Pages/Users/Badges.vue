6afe5d43e9617cc1f5fe1fa480cb43ad651907e2
matiasperalta <matias.peralta@est.fi.uncoma.edu.ar>
2022-11-17 02:53:44 -0300

    title on badges

--- resources/js/Pages/Users/Badges.vue
+++ resources/js/Pages/Users/Badges.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid sm:grid-cols-3 sm:gap-1 md:grid-cols-3 md:gap-2 lg:grid-cols-5 lg:gap-4 xl:gap-0 justify-items-center py-4">
         <div v-for="badge in badges" :key="badge.id">
-            <img :src=badge.imgUrl :alt=badge.name>
+            <img :src=badge.imgUrl :title=badge.name>
         </div>
     </div>
 </template>
